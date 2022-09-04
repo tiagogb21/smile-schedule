fb861d321da28212a75e2320fc8bc8749cda4072
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-09-04 17:48:24 -0300

    build(data): :speech_balloon: constants for project

---
+++ app/backend/src/api/data/data.ts
@@ -0,0 +1,11 @@
+export const incorrectEmailOrPasswordMessage = 'Incorrect email or password';
+
+export const allFieldsMustBeFilledMessage = 'All fields must be filled';
+
+export const incorrectMessageLogin = 'Incorrect username, email or password';
+
+export const incorrectMessageRegister = 'Incorrect username, email or password';
+
+export const invalidToken = 'Token must be a valid token';
+
+export const tokenMustBeValid = 'Token must be a valid token';
